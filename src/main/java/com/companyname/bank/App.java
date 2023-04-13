2de60ca2abdc9f44f625925f631abc72100d9969
Anil kumar <121420630+anildev1@users.noreply.github.com>
2023-04-13 15:58:14 +0530

    code changes

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "New github webhook event test  ");
+        System.out.println( "New code changes ");
     }
 }
